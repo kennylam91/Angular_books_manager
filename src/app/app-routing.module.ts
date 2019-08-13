64fc0d5f17799fc2bc7d73535a6940f6d32e7424
kennylam91 <phamvanlam1991@gmail.com>
2019-08-13 10:35:22 +0700

    add router to book-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {BookListComponent} from './book-list/book-list.component';
 import {BookDetailComponent} from './book-detail/book-detail.component';
+import {BookCreateComponent} from './book-create/book-create.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   {
     path: 'book/:id', component: BookDetailComponent
   },
+  {path: 'book/create', component: BookCreateComponent},
   {path: '**', redirectTo: '/books', pathMatch: 'full'}
 ];
 
