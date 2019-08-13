0977e60cca72e40d596036a5cac214e1b0bf8d48
kennylam91 <phamvanlam1991@gmail.com>
2019-08-13 09:29:29 +0700

    add router to BookListComponent + redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {BookListComponent} from './book-list/book-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'books', component: BookListComponent},
+  {path: '**', redirectTo: '/books', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
