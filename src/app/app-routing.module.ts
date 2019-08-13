9ffaac18c7a1a968c6367e3ba4740e94db2d5202
kennylam91 <phamvanlam1991@gmail.com>
2019-08-13 11:02:42 +0700

    add book/edit router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {Routes, RouterModule} from '@angular/router';
 import {BookListComponent} from './book-list/book-list.component';
 import {BookDetailComponent} from './book-detail/book-detail.component';
 import {BookCreateComponent} from './book-create/book-create.component';
+import {BookEditComponent} from './book-edit/book-edit.component';
 
 
 const routes: Routes = [
@@ -10,11 +11,15 @@ const routes: Routes = [
     path: 'books', component: BookListComponent,
   },
   {path: 'book/create', component: BookCreateComponent},
-
+  {
+    path: 'book/edit/:id', component: BookEditComponent
+  },
   {
     path: 'book/:id', component: BookDetailComponent
   },
-  {path: '**', redirectTo: '/books', pathMatch: 'full'}
+  {
+    path: '**', redirectTo: '/books', pathMatch: 'full'
+  }
 ];
 
 @NgModule({
