3d49fea38f3781936067f7e6d1cb3ea753504f1e
kennylam91 <phamvanlam1991@gmail.com>
2019-08-13 10:29:08 +0700

    add book-create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {AppComponent} from './app.component';
 import {BookListComponent} from './book-list/book-list.component';
 import {HttpClientModule} from '@angular/common/http';
 import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookCreateComponent } from './book-create/book-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookListComponent,
-    BookDetailComponent
+    BookDetailComponent,
+    BookCreateComponent
   ],
   imports: [
     BrowserModule,
