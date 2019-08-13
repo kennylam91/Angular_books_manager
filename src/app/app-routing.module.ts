caa64a9fe2a4000a42e8542f4d986faafbe45514
kennylam91 <phamvanlam1991@gmail.com>
2019-08-13 10:03:00 +0700

    add component: book-detail add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {BookListComponent} from './book-list/book-list.component';
+import {BookDetailComponent} from './book-detail/book-detail.component';
 
 
 const routes: Routes = [
-  {path: 'books', component: BookListComponent},
+  {
+    path: 'books', component: BookListComponent,
+  },
+  {
+    path: 'book/:id', component: BookDetailComponent
+  },
   {path: '**', redirectTo: '/books', pathMatch: 'full'}
 ];
 
