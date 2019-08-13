6c15bd3daf43e23bbe4920d59d567e4653df0fe1
kennylam91 <phamvanlam1991@gmail.com>
2019-08-13 09:32:40 +0700

    add properties: bookList and API_URL to book-service

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {IBook} from './Book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+  private bookList: IBook[];
+  private readonly API_URL = 'http://localhost:3000/books';
 
-  constructor() { }
+
+  constructor() {
+  }
 }
